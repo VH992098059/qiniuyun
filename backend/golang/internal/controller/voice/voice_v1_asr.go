8e6e388ba785090c58822caf8835e0b5ce737bd6
VH992098059 <992098059@qq.com>
2025-10-20 12:41:23 +0800

    feat:更新内容

--- backend/golang/internal/controller/voice/voice_v1_asr.go
+++ backend/golang/internal/controller/voice/voice_v1_asr.go
@@ -2,13 +2,20 @@ package voice
 
 import (
 	"context"
+	login "golang/internal/logic"
 
-	"github.com/gogf/gf/v2/errors/gcode"
-	"github.com/gogf/gf/v2/errors/gerror"
+	"github.com/gogf/gf/v2/frame/g"
 
 	"golang/api/voice/v1"
 )
 
 func (c *ControllerV1) Asr(ctx context.Context, req *v1.AsrReq) (res *v1.AsrRes, err error) {
-	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+	phone, err := login.AsrPhone(ctx, req.AudioBase64)
+	if err != nil {
+		return nil, err
+	}
+	r := g.RequestFromCtx(ctx)
+	r.Response.Header().Set("Content-Type", "audio/mpeg")
+	r.Response.Write(phone)
+	return
 }
--- backend/golang/internal/controller/voice/voice_v1_voice.go
+++ backend/golang/internal/controller/voice/voice_v1_voice.go
@@ -2,13 +2,21 @@ package voice
 
 import (
 	"context"
+	login "golang/internal/logic"
 
-	"github.com/gogf/gf/v2/errors/gcode"
-	"github.com/gogf/gf/v2/errors/gerror"
+	"github.com/gogf/gf/v2/frame/g"
 
 	"golang/api/voice/v1"
 )
 
 func (c *ControllerV1) Voice(ctx context.Context, req *v1.VoiceReq) (res *v1.VoiceRes, err error) {
-	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+	speech, err := login.TextToSpeech(ctx, req.Input)
+	if err != nil {
+		return nil, err
+	}
+	// 直接写入音频数据到响应
+	r := g.RequestFromCtx(ctx)
+	r.Response.Header().Set("Content-Type", "audio/mpeg")
+	r.Response.Write(speech)
+	return &v1.VoiceRes{}, nil
 }
